ea61846f92aaf6d67c231ad6a5a3fea3f461a25a
jhundal01 <jhundal@oswego.edu>
2019-09-19 00:46:52 -0400

    Websocket

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 3030 });
+
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    console.log(`Received message => ${message}`);
+    const data = JSON.parse(message);
+    console.log(`Sending Access token to Client`);
+    ws.send(data.access_token);
+  });
+  console.log("Connected to client");
+});
--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,53 @@
 import React, { Component } from "react";
 import { GoogleLogin } from "react-google-login";
 import config from "./config.json";
+
+const URL = "ws://localhost:3030";
+
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      direct: false
+      direct: false,
+      return: ""
     };
   }
+  ws = new WebSocket(URL);
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log("connected");
+    };
+    this.ws.onmessage = evt => {
+      // on receiving a message, add it to the list of messages
+    //   const message = JSON.parse(evt.data);
+      console.log("Recieved:", evt.data);
+      //   this.addMessage(message);
+    };
+    this.ws.onclose = () => {
+      console.log("disconnected");
+      // automatically try to reconnect on connection loss
+      this.setState({
+        ws: new WebSocket(URL)
+      });
+    };
+  }
+  //   addMessage = message => {
+  //     this.setState(state => ({ return: message }));
+  //     console.log(this.state.return);
+  //   };
+
   signup = res => {
-    const { direct } = this.state;
+    const { direct, data } = this.state;
+    console.log("Sent:", res.Zi);
     this.setState({
       direct: true
     });
+    this.ws.send(JSON.stringify(res.Zi));
   };
   render() {
     const { direct } = this.state;
     const responseGoogle = response => {
-      console.log(response);
       this.signup(response);
     };
     return (
